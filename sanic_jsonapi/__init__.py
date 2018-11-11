98e2b9adf039bc69e3ee0a903f2e3a5d3d69d7b4
Lucifer Avada <lucifer.avada@gmail.com>
2018-11-10 21:16:56 -0500

    do not import rethinkdb classes

--- sanic_jsonapi/__init__.py
+++ sanic_jsonapi/__init__.py
@@ -4,5 +4,3 @@ from . field import Field
 from . modelmeta import ModelMeta
 from . model import Model, Error
 from . jsonapi import JSONAPIMixin, Empty, jsonapi
-
-from . backend.rethinkdb import RethinkDB, RethinkDBModel
