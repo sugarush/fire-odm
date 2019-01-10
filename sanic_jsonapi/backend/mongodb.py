6c74bcae3b80acf9b0be6948e1e3b3665cde78b8
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-10 18:09:29 -0500

    add and test MongoDBModel methods

--- sanic_jsonapi/backend/mongodb.py
+++ sanic_jsonapi/backend/mongodb.py
@@ -1,6 +1,7 @@
 import inflection
 from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import ReturnDocument
 
 from .. util import serialize
 from .. model import Model, Field, Error
@@ -25,30 +26,22 @@ class MongoDB(object):
 
 class MongoDBModel(Model):
 
-    connection_options = { }
-    connection = None
-
-    database_options = { }
-    database = None
-
-    collection_options = { }
-    collection = None
-
     @classmethod
     def initialize(cls):
         if cls.__name__ == 'MongoDBModel':
             return
 
+        if not hasattr(cls, 'connection_options'):
+            cls.connection_options = { }
+
+        if not hasattr(cls, 'database_options'):
+            cls.database_options = { 'name': 'test' }
+
+        if not hasattr(cls, 'collection_options'):
+            cls.collection_options = { 'name': cls._table }
+
         cls.connection = MongoDB.connect(**cls.connection_options)
-
-        if not cls.database_options.get('name'):
-            cls.database_options['name'] = 'test'
-
         cls.database = cls.connection.get_database(**cls.database_options)
-
-        if not cls.collection_options.get('name'):
-            cls.collection_options['name'] = cls._table
-
         cls.collection = cls.database.get_collection(**cls.collection_options)
 
     @classmethod
@@ -56,7 +49,7 @@ class MongoDBModel(Model):
         field = Field()
         field.name = '_id'
         field.primary = True
-        field.type = ObjectId
+        field.type = str
         return field
 
     @classmethod
@@ -64,36 +57,135 @@ class MongoDBModel(Model):
         if not primary.name is '_id':
             raise AttributeError('MongoDBModel primary key name must be: _id')
 
-        if not primary.type is ObjectId:
+        if not primary.type is str:
             raise AttributeError('MongoDBModel primary key type must be: ObjectId')
 
     @classmethod
-    async def find(cls, query={ }, projection={ }):
-        raise NotImplementedError()
+    async def exists(cls, id):
+        if id:
+            document = await cls.collection.find_one(
+                { '_id': ObjectId(id) },
+                { '_id': True }
+            )
+            if document:
+                return True
+            return False
+        else:
+            raise Error(
+                title = 'Document Exists Failed',
+                detail = 'No document ID provided.'
+            )
 
     @classmethod
     async def find_one(cls, id):
-        raise NotImplementedError()
+        if id:
+            document = await cls.collection.find_one(
+                { '_id': ObjectId(id) }
+            )
+            if document:
+                return cls(document)
+            else:
+                raise Error(
+                    title = 'Document Find One Failed',
+                    detail = 'No document returned.'
+                )
+        else:
+            raise Error(
+                title = 'Document Find One Failed',
+                detail = 'No document ID provided.'
+            )
 
     @classmethod
-    async def exists(cls, id):
-        raise NotImplementedError()
+    async def find(cls, *args, **kargs):
+        cursor = cls.collection.find(*args, **kargs)
+        async for document in cursor:
+            yield cls(document)
 
     @classmethod
-    async def add(cls, *args):
-        raise NotImplementedError()
+    async def add(cls, args):
+        if isinstance(args, dict):
+            model = cls(args)
+            await model.save()
+            return model
+        elif isinstance(args, list):
+            models = [ ]
+            for data in args:
+                model = cls(data)
+                await model.save()
+                models.append(model)
+            return models
+        else:
+            raise Error(
+                title = 'Document Add Failed',
+                detail = 'Invalid argument to MongoDBModel.add: must be a list or dict.'
+            )
 
     async def save(self):
-        try:
-            self.validate()
-        except Error as error:
-            return error
+        self.validate()
 
-        data = self.serialize(computed=True, controllers=True, reset=True)
-        result = await self.collection.insert_one(data)
+        if self.id:
+            data = self.serialize(computed=True, controllers=True, reset=True)
+            del data['_id']
+            document = await self.collection.find_one_and_update(
+                { '_id': ObjectId(self.id) },
+                { '$set': data },
+                return_document=ReturnDocument.AFTER
+            )
+            if document:
+                self.update(document)
+            else:
+                raise Error(
+                    title = 'Document Save Failed',
+                    detail = 'No document returned.'
+                )
+        else:
+            data = self.serialize(computed=True, controllers=True, reset=True)
+            result = await self.collection.insert_one(data)
+            if result:
+                self.id = result.inserted_id
+                await self.load()
+            else:
+                raise Error(
+                    title = 'Document Save Failed',
+                    detail = 'Inserted ID not available or non-existent.'
+                )
 
     async def load(self):
-        raise NotImplementedError()
+        if self.id:
+            document = await self.collection \
+                .find_one({ '_id': ObjectId(self.id) })
+            if document:
+                self.update(document)
+            else:
+                raise Error(
+                    title = 'Document Load Failed',
+                    detail = 'No document returned.'
+                )
+        else:
+            raise Error(
+                title = 'Document Load Failed',
+                detail = 'No document ID, cannot load.'
+            )
 
     async def delete(self):
-        raise NotImplementedError()
+        if self.id:
+            result = await self.collection \
+                .delete_one({ '_id': ObjectId(self.id) })
+            if result:
+                if result.deleted_count == 0:
+                    raise Error(
+                        title = 'Document Delete Failed',
+                        detail = 'Deleted count is zero.'
+                    )
+                else:
+                    self._data = { }
+            else:
+                raise Error(
+                    title = 'Document Delete Failed',
+                    detail = 'Collection operation result is a falsy value.'
+                )
+        else:
+            raise Error(
+                title = 'Document Delete Failed',
+                detail = 'No document ID, cannot delete.'
+            )
--- test/test_mongodb.py
+++ test/test_mongodb.py
@@ -1,7 +1,5 @@
 from unittest import TestCase
 
-from bson import ObjectId
-
 from sanic_jsonapi import AsyncTestCase, Field
 from sanic_jsonapi.backend.mongodb import MongoDB, MongoDBModel
 
@@ -18,11 +16,13 @@ class MongoDBTest(TestCase):
 
 class MongoDBModelTest(AsyncTestCase):
 
+    default_loop = True
+
     def test_default_primary(self):
 
         field = MongoDBModel.default_primary()
 
-        self.assertIs(field.type, ObjectId)
+        self.assertIs(field.type, str)
         self.assertEqual(field.name, '_id')
         self.assertTrue(field.primary)
 
@@ -36,7 +36,7 @@ class MongoDBModelTest(AsyncTestCase):
 
     def test_check_primary_type(self):
 
-        field = Field(type=str)
+        field = Field(type=int)
         field.name = '_id'
 
         with self.assertRaises(AttributeError):
@@ -114,11 +114,124 @@ class MongoDBModelTest(AsyncTestCase):
 
         self.assertEqual(Test.collection.name, 'some_collection')
 
-    def test_save_new(self):
-        pass
+    async def test_save_new(self):
 
-    def test_save_existing(self):
-        pass
+        class Test(MongoDBModel):
+            pass
 
-    def test_exists(self):
-        pass
+        test = Test()
+
+        await test.save()
+
+        self.assertTrue(test.id)
+
+        await test.delete()
+
+    async def test_save_existing(self):
+
+        class Test(MongoDBModel):
+            field = Field()
+
+        test = Test()
+        test.field = 'test'
+
+        await test.save()
+
+        self.assertEqual(test.field, 'test')
+
+        test.field = 'test2'
+
+        await test.save()
+
+        self.assertEqual(test.field, 'test2')
+
+        await test.delete()
+
+    async def test_delete(self):
+
+        class Test(MongoDBModel):
+            field = Field()
+
+        test = Test()
+        test.field = 'test'
+
+        await test.save()
+        await test.delete()
+
+        self.assertDictEqual(test._data, { })
+
+    async def test_exists(self):
+
+        class Test(MongoDBModel):
+            pass
+
+        test = Test()
+        await test.save()
+
+        id = test.id
+
+        self.assertTrue(await test.exists(id))
+
+        await test.delete()
+
+        self.assertFalse(await test.exists(id))
+
+    async def test_find_one(self):
+
+        class Test(MongoDBModel):
+            pass
+
+        test = Test()
+        await test.save()
+
+        id = test.id
+
+        test = await Test.find_one(id)
+
+        self.assertEqual(test.id, id)
+
+        await test.delete()
+
+    async def test_find(self):
+
+        class Test(MongoDBModel):
+            pass
+
+        test1 = Test()
+        await test1.save()
+
+        test2 = Test()
+        await test2.save()
+
+        ids = [ ]
+
+        async for model in Test.find():
+            ids.append(model.id)
+            await model.delete()
+
+        self.assertIn(test1.id, ids)
+        self.assertIn(test2.id, ids)
+
+    async def test_add_single(self):
+
+        class Test(MongoDBModel):
+            pass
+
+        model = await Test.add({ })
+
+        self.assertTrue(model.id)
+
+        await model.delete()
+
+    async def test_add_multiple(self):
+
+        class Test(MongoDBModel):
+            pass
+
+        models = await Test.add([
+            { },
+            { },
+            { }
+        ])
+
+        self.assertEqual(len(models), 3)
