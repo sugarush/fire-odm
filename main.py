f639619b9ded9a06676329fde77e8e6af14c1e10
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-17 11:23:57 -0500

    change to work with MongoDBModel

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import asyncio
 from user import User
 
 async def seed():
-    models, errors = await User.add([
+    
+    models = await User.add([
         {
-            'id': '1',
             'name': {
                 'first': 'paul',
                 'last': 'severance'
             }
         },
         {
-            'id': '2',
             'name': {
                 'first': 'alice',
                 'last': 'jane'
@@ -20,24 +19,15 @@ async def seed():
         }
     ])
 
-    if models:
-        for model in models:
-            print(model)
-
-    if errors:
-        for error in errors:
-            print(error)
-
-async def main(loop):
+async def main():
 
     await User.drop()
 
     await seed()
 
     async for user in User.find():
-        await user.save()
-        #print(user.to_jsonapi())
+        print(user)
+        await user.delete()
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-loop.run_until_complete(main(loop))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
--- user.py
+++ user.py
@@ -1,23 +1,15 @@
 from datetime import datetime
 
-import rethinkdb as r
+from sanic_jsonapi import MongoDBModel, Field
 
-from sanic_jsonapi import RethinkDBModel, Field
-
-class Name(RethinkDBModel):
+class Name(MongoDBModel):
     first = Field(required=True)
     last = Field(required=True)
 
-class User(RethinkDBModel):
-    #id = Field(type=int, primary=True)
+class User(MongoDBModel):
     name = Field(type=Name)
-    updated = Field(type='set_timestamp', computed='timestamp')
-    created = Field(type='set_timestamp', computed='timestamp', computed_empty=True)
+    updated = Field(computed='timestamp')
+    created = Field(computed='timestamp', computed_empty=True)
 
     def timestamp(self):
-        return r.now()
-
-    def set_timestamp(self, value):
-        if isinstance(value, datetime):
-            return value.isoformat()
-        return value
+        return datetime.utcnow().timestamp()
