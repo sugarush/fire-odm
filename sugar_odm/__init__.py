2fba76de16fb67569b546c4df54a0c8a5d7d0e79
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-17 18:06:06 -0500

    remove jsonapi

--- sugar_odm/__init__.py
+++ sugar_odm/__init__.py
@@ -3,6 +3,5 @@ from . asynctest import AsyncTestCase
 from . field import Field
 from . modelmeta import ModelMeta
 from . model import Model, Error
-from . jsonapi import JSONAPIMixin, Empty, jsonapi
 
 from . backend.mongodb import MongoDBModel
