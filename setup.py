08710ada368472b2d3b01671c984b64955107704
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-17 19:39:28 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+__author__ = 'Lucifer Avada'
+
+from setuptools import setup
+
+setup(
+    name='sugar-odm',
+    version='0.0.1',
+    author='Lucifer Avada',
+    author_email='lucifer.avada@gmail.com',
+    url='https://github.com/sugarush/sugar-odm',
+    packages=['sugar_odm'],
+    description='An async ODM for MongoDB.',
+    install_requires=[
+        'sugar_asynctest'
+    ],
+    dependency_links=[
+        'git+https://github.com/sugarush/sugar-asynctest@f652ade886214ecb74beeb39f6fb5533e03305ed#egg=sugar_asynctest'
+
+    ]
+)
