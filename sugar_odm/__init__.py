7790825718b1f562d8c5889c6c1f5331eb46e5e9
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-20 23:57:57 -0500

    import MemoryModel

--- sugar_odm/__init__.py
+++ sugar_odm/__init__.py
@@ -2,4 +2,5 @@ from . field import Field
 from . modelmeta import ModelMeta
 from . model import Model
 
+from . backend.memory import MemoryModel
 from . backend.mongodb import MongoDB, MongoDBModel
