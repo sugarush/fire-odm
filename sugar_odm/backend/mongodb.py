f134cfb7d5092f6ae90548bad6d315cb9d2c40a5
Lucifer Avada <lucifer.avada@gmail.com>
2019-08-10 20:11:36 -0400

    await MongoDBModel.exists

--- sugar_odm/backend/mongodb.py
+++ sugar_odm/backend/mongodb.py
@@ -165,7 +165,7 @@ class MongoDBModel(Model, RelationshipMixin):
     async def save(self):
         self._connect()
         self.validate()
-        if self.id and self.exists(self.id):
+        if self.id and await self.exists(self.id):
             data = self.serialize(computed=True, reset=True)
             del data['_id']
             document = await self._collection.find_one_and_update(
