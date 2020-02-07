5ddce111e4082684621880666b23c40515cafa1d
Paul Severance <paul@sugarush.io>
2020-02-06 20:37:01 -0500

    remove versions

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ setup(
         'fire_odm.backend',
         'fire_odm.controller'
     ],
-    description='An async ODM for MongoDB and Postgres.',
+    description='An asynchronous ODM for MongoDB and Postgres.',
     install_requires=[
-        'inflection==0.3.1',
-        'motor==2.0.0',
-        'ujson==1.35',
-        'asyncpg==0.20.1',
         'fire-asynctest@git+https://github.com/sugarush/fire-asynctest@master',
+        'inflection',
+        'motor',
+        'ujson',
+        'asyncpg',
         'ipython'
     ]
 )
